6e31eef901fd7e5a6a12eb61d05e8f5de7db61ff
Kwuang Tang <10319942+cktang88@users.noreply.github.com>
2022-04-23 00:48:49 +0000

    update vite

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,45 @@
-import * as React from "react";
-// 1. import 'ChakraProvider' component
-import { Box, ChakraProvider, Divider, Flex } from "@chakra-ui/react";
-import SignupForm from "./features/form";
-import DataTable from "./features/table";
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
 function App() {
-  // 2. Use at the root of your app
+  const [count, setCount] = useState(0)
+
   return (
-    <ChakraProvider>
-      <Flex flexDirection="column">
-        <Box mx="auto" py={2} px={8}>
-          <SignupForm />
-        </Box>
-        <Divider m={8} />
-        <Box mx="auto" py={2} px={8}>
-          <DataTable />
-        </Box>
-      </Flex>
-    </ChakraProvider>
-  );
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Hello Vite + React!</p>
+        <p>
+          <button type="button" onClick={() => setCount((count) => count + 1)}>
+            count is: {count}
+          </button>
+        </p>
+        <p>
+          Edit <code>App.tsx</code> and save to test HMR updates.
+        </p>
+        <p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          {' | '}
+          <a
+            className="App-link"
+            href="https://vitejs.dev/guide/features.html"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Vite Docs
+          </a>
+        </p>
+      </header>
+    </div>
+  )
 }
 
-export default App;
+export default App
