3767cbe36f99c74159ab74ea28d075c19d7716f5
cktang88 <arctangent20@yahoo.com>
2022-07-18 23:51:57 -0500

    add react router

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import Expenses from "./routes/expenses";
+import Invoices from "./routes/invoices";
 
 // Start the mocking conditionally.
 if (process.env.NODE_ENV === "development") {
@@ -12,6 +15,12 @@ if (process.env.NODE_ENV === "development") {
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="expenses" element={<Expenses />} />
+        <Route path="invoices" element={<Invoices />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
